55565bcdbb22bb7a3f8028c54cb9e112f81cd56f
Ludovic NAULOT <pctronique@free.fr>
2023-02-17 11:05:18 +0100

    modifier le header

--- project/www/src/pages/header.php
+++ project/www/src/pages/header.php
@@ -198,8 +198,4 @@
     </script>
     <!-- partial -->
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
-<<<<<<< HEAD
-    <script src="./js/script.js"></script>
-=======
     <script src="/src/js/script.js"></script>
->>>>>>> vaness
